a85cb598124e9f4a49a7a1d72eadfc784c43b68b
AngelaBello-creator <angelabellomedina@gmail.com>
2025-09-23 09:56:47 +0200

    initialize board class

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,16 +1,20 @@
-package main.java;
-
 public class Board {
 
-    board = new char [3] [3];
-    initializeBoard();  
-}
+    private char[][] board;
 
-public void initializeBoard() {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            board [i] [j] = '-';
+
+    public Board() {
+        board = new char[3][3];
+        initializeBoard();
+    }
+
+  
+    public void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
         }
     }
-}
 
+}
