601f7ca50c8cef3903b2c8137f10b7d119b7e979
Daniella Pacheco <daniellak.pacheco@gmail.com>
2025-09-23 10:43:16 +0200

    Implement 3x3 Board class for tic-tac-toe with display method

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,14 +1,11 @@
 public class Board {
-
     private char[][] board;
-
 
     public Board() {
         board = new char[3][3];
         initializeBoard();
     }
 
-  
     public void initializeBoard() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -17,4 +14,12 @@ public class Board {
         }
     }
 
+    public void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
